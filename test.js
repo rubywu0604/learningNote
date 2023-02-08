86d03d706ad8bbbd391292c21bd74131d52f1f10
RubyWu <lksh20602@gmail.com>
2023-02-09 00:23:47 +0800

    add PrototypesAndClasses.js, update readme

--- test.js
+++ test.js
@@ -1,13 +1,20 @@
 
-let deck = [1, 2, 3, 4];
+    console.log(Math.pow(-2,31));
 
-function threeOfEachThree(deck) {
-  deck.reduce((accumulator, currentValue) => {
-    if (currentValue === 3){
-      accumulator.deck.push(currentValue);
+console.log(Math.pow(-2,31) <= 1 <= (Math.pow(2,31)-1) ? 0 : false);
+
+var reverse = function(x) {
+    const str = x.toString();
+    let ary = [];
+    for(var i = str.length - 1; i >= 0; i--){
+        if(str[i] === "-"){
+            ary.unshift(str[i]);
+        }else{
+            ary.push(str[i]);
+        };
     };
-    return accumulator;
-  })
-}
+    var reversedX = Number(ary.join(''));
+    return (Math.pow(-2,31) <= reversedX <= (Math.pow(2,31)-1) ? reversedX : 0
+    }
 
-console.log(threeOfEachThree(deck);
+console.log(Math.pow(-2,31) <= reversedX <= (Math.pow(2,31)-1) ? reversedX : 0);
