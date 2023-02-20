5cfff9d05b17770b6537c09252be9d677af3806c
RubyWu <lksh20602@gmail.com>
2023-02-20 17:46:22 +0800

    css style on port success

--- ProjectProfile/profile.js
+++ ProjectProfile/profile.js
@@ -4,13 +4,22 @@ const app = express();
 const port = 8080;
 
 // Static files
-app.use(express.static("webpage"));
+app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
 app.use("/img", express.static(__dirname + "public/img"));
 app.use("/js", express.static(__dirname + "public/js"));
 
-app.get("", function (req, res) {
-  res.sendFile(__dirname + "/webpage/home.html");
+
+// Set webpage
+app.set("views", "./views")
+app.set("view engine", "ejs")
+
+app.get("/home.html", function (req, res) {  // open with "http://localhost:8080/home.html"
+  res.render("home");
+})
+
+app.get("/love.html", function (req, res) {  // open with "http://localhost:8080/love.html"
+  res.render("love");
 })
 
 // Listen on port
