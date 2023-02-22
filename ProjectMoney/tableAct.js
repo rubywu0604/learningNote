953a371fbc505867e8c895623f4367c6ab8904af
RubyWu <lksh20602@gmail.com>
2023-02-22 15:25:34 +0800

    fix: show string without background height changed

--- ProjectMoney/tableAct.js
+++ ProjectMoney/tableAct.js
@@ -1,5 +1,5 @@
 // note will appear 1 second after loading
-window.onload = function(){
+window.onload = function (){
     setTimeout(() => {
     //We use window.onload to check the window has loaded so we can target DOM elements
     var noteArray = ["please remember to record your spending and earnings ", "<b>EVERYDAY</b>"];
@@ -8,6 +8,7 @@ window.onload = function(){
     document.getElementById("note").innerHTML = note;
   }, 1000);
 }
+
 //user click the "html" button will trigger the "js" doc to invoke the function
 document.getElementById('click').onclick = function sum() {
   var item1 = parseInt(document.getElementById('item1').value);
