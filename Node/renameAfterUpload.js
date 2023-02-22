5a1cfd4f8c6ecb3ab3e2a54255b0923cb3d12d93
RubyWu <lksh20602@gmail.com>
2023-02-23 01:19:44 +0800

    Node.js Upload Files

--- Node/renameAfterUpload.js
+++ Node/renameAfterUpload.js
@@ -7,7 +7,7 @@ http.createServer(function (req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
       var oldpath = files.filetoupload.filepath;
-      var newpath = 'C:/Users/ruby/' + files.filetoupload.originalFilename;
+      var newpath = '/Users/ruby/' + files.filetoupload.originalFilename;
       fs.rename(oldpath, newpath, function (err) {
         if (err) throw err;
         res.write('File uploaded and moved!');
