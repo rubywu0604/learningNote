c3c4c3dcdf7e6d37e158e4b9a5102adad5f2a502
RubyWu <lksh20602@gmail.com>
2023-03-07 12:04:54 +0800

    server can receive request

--- javascript/server-side/index.js
+++ javascript/server-side/index.js
@@ -6,4 +6,14 @@ const app = express();
 const port = 8080;
 
 app.listen(port, () => console.log(`listen on the port: ${port}`));
+//1. host static file (index.html)
 app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+// to see whether the endpoint('api') can receive the client's request or not
+// request: server get all info from client's
+// response: server sent thing back to the client base on their req
+app.post('/api', (request, response) => {
+  console.log('I got a request!')
+  console.log(request.body); // receive the POST package(combine data)
+})
