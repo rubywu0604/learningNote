232bdc148170d5c3b987f6f8c3f95ddda36696e2
RubyWu <lksh20602@gmail.com>
2023-03-08 19:00:33 +0800

    fetch data from database and show on the webpage

--- javascript/server-side/index.js
+++ javascript/server-side/index.js
@@ -5,29 +5,38 @@ const express = require('express');
 const fs = require('fs')
 const app = express();
 const port = 8080;
+const Datastore = require('nedb')
 
 app.listen(port, () => console.log(`listen on the port: ${port}`));
 //1. host static file (index.html)
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 
-const database = [];
+const source = new Datastore('index.db');
+source.loadDatabase();
+
 // to see whether the endpoint('api') can receive the client's request or not
 // request: server get all info from client's
 // response: server sent thing back to the client base on their req
 app.post('/api', (request, response) => {
-  console.log('I got a request! File updated as below.')
-  const location = request.body;
-  database.push(location);
-  console.log(database);
-
-  fs.writeFile('data.txt', JSON.stringify(database), function (err) {
-    if (err) throw err;
-  });
+  console.log('I got a request!')
+  const data = request.body;
+  const timestamp = Date.now();
+  data.time = timestamp;
+  source.insert(data);
+  console.log(data);
  // receive the POST package(combine data)
-  response.json({
-    status: 'success',
-    latitude: location.lat,
-    loogitude: location.lon,
-  });
+  response.json(data);
 });
+
+
+app.get('/api', (request, response) => {
+  console.log('here is the data')
+  source.find({}, function (err, source) {
+    if(err) {
+      response.end();
+      return;
+    };
+    response.json(source);
+  });
+})
