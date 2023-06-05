a8556a5bce861ecb1b05263ca592dec7648cac04
rubywu0604 <lksh20602@gmail.com>
2023-06-05 18:21:47 +0800

    5/26-6/5 daily update

---
+++ python/test.py
@@ -0,0 +1,19 @@
+# Note
+# print("HELLO")
+
+# name = input("What is your name? ")
+# print("Hi " + name)
+
+# def two_sum(a, b) :
+#     """summerize two int
+#     :param a: int
+#     :param b: int
+#     takes a and b to sum the result
+#     """
+
+#     return a + b
+
+# print(two_sum.__doc__)
+
+s = set("Hello")
+print(s)
