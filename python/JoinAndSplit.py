0545d2fc60f484007a6c62de2cc5b79123e0ad5a
rubywu0604 <lksh20602@gmail.com>
2023-06-25 18:04:27 +0800

    add file: JoinAndSplit.py

---
+++ python/JoinAndSplit.py
@@ -0,0 +1,11 @@
+# Join
+color = ['red', 'yellow', 'pink', 'orange', 'blue', 'purple']
+join_string = '/'.join(color)
+
+print(type(join_string), join_string)
+
+# Split
+fruit = 'I like to eat apple and banana.'
+split_list = fruit.split(' ')
+
+print(type(split_list), split_list)
