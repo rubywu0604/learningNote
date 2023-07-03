2a9393f276d60d9f9561e2a9430b17c9da0f8b3b
rubywu0604 <lksh20602@gmail.com>
2023-07-03 17:56:10 +0800

    update packing and unpacking sample

--- python/dict.py
+++ python/dict.py
@@ -8,3 +8,24 @@ for color in a:
     b[color] = b.get(color, 0) + a[color]
 
 print(b)
+
+# packing dict ----------------------
+def packing_me(**kwargs):
+    for key, value in kwargs.items():
+        print(f'{key}. {value}')
+
+packing_me(name = 'ruby', age = 31, gender = 'female')
+
+# unpacking dict ----------------------
+me = {
+    'name': 'ruby',
+    'age': 31,
+    'gender': 'female'
+}
+
+def unpacking_me(name, age, gender):
+    print(name)
+    print(age)
+    print(gender)
+
+unpacking_me(**me)
